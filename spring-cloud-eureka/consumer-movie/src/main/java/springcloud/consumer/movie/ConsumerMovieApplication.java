2d4118075aa7983732db94baa8056eeaaa8d74f1
changhao <www.changhao@outlook.com>
2018-12-15 23:35:22 +0800

    Eureka注册配置

---
+++ spring-cloud-eureka/consumer-movie/src/main/java/springcloud/consumer/movie/ConsumerMovieApplication.java
@@ -0,0 +1,17 @@
+package springcloud.consumer.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+//@EnableDiscoveryClient
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMovieApplication.class, args);
+    }
+
+}
+
---
+++ spring-cloud-eureka/eureka-server-master/src/main/java/springcloud/eureka/server/EurekaMasterServerApplication.java
@@ -0,0 +1,16 @@
+package springcloud.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaMasterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMasterServerApplication.class, args);
+    }
+
+}
+
